8003c3d5014ed7b66b1365648f8be7138735e2ca
Raymond Yeh <ray@geek.sg>
2020-04-29 15:01:29 +0800

    feat: clonable title escrow and creator

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {TitleEscrowFactory} from "./contracts/TitleEscrowFactory";
 export {TradeTrustERC721Factory} from "./contracts/TradeTrustERC721Factory";
+export {TitleEscrowClonableFactory} from "./contracts/TitleEscrowClonableFactory";
+export {TitleEscrowCreatorFactory} from "./contracts/TitleEscrowCreatorFactory";
