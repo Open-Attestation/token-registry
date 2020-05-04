7ffe6e275dc4bcaa505cceb9eede92978c134aa5
Raymond Yeh <ray@geek.sg>
2020-05-04 12:17:07 +0800

    fix: test for TitleEscrowCreatorFactory

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export {TitleEscrowFactory} from "./contracts/TitleEscrowFactory";
 export {TradeTrustERC721Factory} from "./contracts/TradeTrustERC721Factory";
-export {TitleEscrowClonableFactory} from "./contracts/TitleEscrowClonableFactory";
 export {TitleEscrowCreatorFactory} from "./contracts/TitleEscrowCreatorFactory";
