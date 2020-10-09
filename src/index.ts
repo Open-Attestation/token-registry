04df169b3726ed0639eb88b8bb93e366eb8ff606
Justus Soh <45728710+justussoh@users.noreply.github.com>
2020-10-09 11:49:05 +0800

    feat: added function to call TitleEscrowCreator Address (#16)
    * feat: added function to call TitleEscrowCreator Address
    
    * fix: removed loading from process.env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {TitleEscrowFactory} from "./contracts/TitleEscrowFactory";
 export {TradeTrustErc721Factory} from "./contracts/TradeTrustErc721Factory";
 export {TitleEscrowCreatorFactory} from "./contracts/TitleEscrowCreatorFactory";
+export {getTitleEscrowCreatorAddress} from "./config";
