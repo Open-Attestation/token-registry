233fa868058ee71d2496713c1fcb14e4789d4db5
superical <superical@users.noreply.github.com>
2021-09-27 11:29:09 +0800

    feat: restore add TitleEscrowCloneable (#32) (#34)
    BREAKING CHANGE: changes in method names in token registry and removal of title escrow creator

--- contracts/ITitleEscrowCreator.sol
+++ contracts/ITitleEscrowCreator.sol
@@ -1,7 +1,19 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
 
 /// @title Title Escrow for Transferable Records
 interface ITitleEscrowCreator {
+  event TitleEscrowDeployed(
+    address indexed escrowAddress,
+    address indexed tokenRegistry,
+    address beneficiary,
+    address holder
+  );
+
   /// @notice Deploys an instance of a title escrow
-  function deployNewTitleEscrow(address tokenRegistry, address beneficiary, address holder) external returns (address);
+  function deployNewTitleEscrow(
+    address tokenRegistry,
+    address beneficiary,
+    address holder
+  ) external returns (address);
 }
---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-watcher");
+require("hardhat-typechain");
+require("hardhat-gas-reporter");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  typechain: {
+    outDir: "src/contracts",
+  },
+  watcher: {
+    test: {
+      tasks: ["compile", "test"],
+      files: ["./contracts", "./test"],
+    },
+  },
+  gasReporter: {
+    coinmarketcap: "e3dd106b-9644-45ac-8f61-98614b973bca",
+    currency: "USD",
+  },
+};
