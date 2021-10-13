bd8be25fffa08cac72cf14c59ee1e6d5c35b4ac2
Seth <hullogh@gmail.com>
2021-10-13 19:10:26 +0800

    test: add getTestUsers in test utils

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,4 +1,6 @@
-import { ContractTransaction, ethers } from "ethers";
+import { ethers } from "hardhat";
+import { ContractTransaction } from "ethers";
+import { TestUsers } from "../fixtures/deploy-token.fixture";
 
 export const getEventFromTransaction = async (tx: ContractTransaction, abi: string[], eventName: string) => {
   const iface = new ethers.utils.Interface(abi);
@@ -11,4 +13,10 @@ export const getEventFromTransaction = async (tx: ContractTransaction, abi: stri
   if (!event) throw new Error(`Cannot find event ${eventName}`);
 
   return iface.parseLog(event).args;
+};
+
+export const getTestUsers = async () => {
+  const [carrier, beneficiary, holder, ...others] = await ethers.getSigners();
+  const users: TestUsers = { carrier, beneficiary, holder, others };
+  return users;
 };
