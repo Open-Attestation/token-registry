e5021442f5c09f8cccfcf6f1ad51210012fffb47
Seth <hullogh@gmail.com>
2021-11-17 19:56:51 +0800

    feat(deployment): refactor hh config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,30 @@ import "hardhat-watcher";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-etherscan";
-import { HardhatUserConfig } from "hardhat/types";
+import dotenv from "dotenv";
+import { HardhatUserConfig, HttpNetworkUserConfig } from "hardhat/types";
 import "./tasks";
+
+dotenv.config();
+
+const { INFURA_API_KEY, MNEMONIC, DEPLOYER_PK, COINMARKETCAP_API_KEY, ETHERSCAN_API_KEY } = process.env;
+
+if (!INFURA_API_KEY) {
+  throw new Error("Infura key is not provided in env");
+}
+
+if (!DEPLOYER_PK && !MNEMONIC) {
+  throw new Error("Provide at least either deployer private key or mnemonic in env");
+}
+
+const networkConfig: HttpNetworkUserConfig = {};
+if (DEPLOYER_PK) {
+  networkConfig.accounts = [DEPLOYER_PK];
+} else {
+  networkConfig.accounts = {
+    mnemonic: MNEMONIC!,
+  };
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -34,11 +56,59 @@ const config: HardhatUserConfig = {
     },
   },
   gasReporter: {
-    coinmarketcap: process.env.COINMARKETCAP_TOKEN,
+    coinmarketcap: COINMARKETCAP_API_KEY,
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    /**
+     * Ethereum
+     */
+    mainnet: {
+      ...networkConfig,
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    ropsten: {
+      ...networkConfig,
+      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    goerli: {
+      ...networkConfig,
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      ...networkConfig,
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    kovan: {
+      ...networkConfig,
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    /**
+     * Polygon
+     */
+    polygon: {
+      ...networkConfig,
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+    },
+    mumbai: {
+      ...networkConfig,
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
+      // url: "https://matic-mumbai.chainstacklabs.com",
+    },
+    /**
+     * Development
+     */
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      ...networkConfig,
+      accounts: "remote",
+      url: "http://127.0.0.1:8545",
+    },
   },
 };
 
