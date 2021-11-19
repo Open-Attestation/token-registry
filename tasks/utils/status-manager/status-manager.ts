50e83a7f6d3a94c253ac27df80f0183c59266ecd
Seth <hullogh@gmail.com>
2021-11-20 01:58:13 +0800

    refactor(deployment): add options to status manager

--- tasks/utils/status-manager/status-manager.ts
+++ tasks/utils/status-manager/status-manager.ts
@@ -1,4 +1,4 @@
-import Spinner from "spinnies";
+import Spinner, { SpinnerOptions } from "spinnies";
 import { v4 as uuid } from "uuid";
 
 export class StatusManager {
@@ -13,33 +13,33 @@ export class StatusManager {
     this.name = targetName;
   }
 
-  public add(msg: string) {
-    this.spinner.add(this.name, { text: msg });
+  public add(msg: string, opts?: SpinnerOptions) {
+    this.spinner.add(this.name, { ...opts, text: msg });
   }
 
-  public update(msg: string) {
-    this.spinner.update(this.name, { text: msg });
+  public update(msg: string, opts?: SpinnerOptions) {
+    this.spinner.update(this.name, { ...opts, text: msg });
   }
 
   public remove() {
     this.spinner.remove(this.name);
   }
 
-  public succeed(msg: string) {
-    this.spinner.succeed(this.name, { text: msg });
+  public succeed(msg: string, opts?: SpinnerOptions) {
+    this.spinner.succeed(this.name, { ...opts, text: msg });
   }
 
-  public fail(msg: string) {
-    this.spinner.fail(this.name, { text: msg });
+  public fail(msg: string, opts?: SpinnerOptions) {
+    this.spinner.fail(this.name, { ...opts, text: msg });
   }
 
   public stopAll(status: "succeed" | "fail" | "stopped") {
     this.spinner.stopAll(status);
   }
 
-  public static create(): StatusManager {
+  public static create(opts?: SpinnerOptions): StatusManager {
     if (!this.spinner) {
-      this.spinner = new Spinner({ color: "blue" });
+      this.spinner = new Spinner({ color: "blue", ...opts });
     }
     const name = uuid();
     return new StatusManager(this.spinner, name);
