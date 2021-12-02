8605eb3e3524640c9cbcbe93f32b4e9460256d20
superical <superical@users.noreply.github.com>
2021-12-02 13:46:37 +0800

    chore: convert to typescript setup (#43)

---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-watcher";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"]
+        }
+      }
+    }
+  },
+  typechain: {
+    outDir: "src/contracts",
+    alwaysGenerateOverloads: true
+  },
+  watcher: {
+    test: {
+      tasks: ["compile", "test"],
+      files: ["./contracts", "./test"]
+    }
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_TOKEN,
+    currency: "USD"
+  },
+};
+
+export default config;
