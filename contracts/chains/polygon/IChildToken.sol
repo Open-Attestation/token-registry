2d2aff79fb3ceed046cd87395a08da94de498b9a
Seth <hullogh@gmail.com>
2022-01-22 14:54:29 +0800

    feat: remove all chain manager contracts and tests

--- contracts/chains/polygon/IChildToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
-
-interface IChildToken {
-  function deposit(address depositor, uint tokenId, bytes memory depositData) external;
-
-  function withdraw(address withdrawer, uint tokenId, bytes memory withdrawData) external;
-}
--- contracts/mocks/chains/polygon/FxChildMock.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
-
-import { FxChild } from "@maticnetwork/fx-portal/contracts/FxChild.sol";
-
-contract FxChildMock is FxChild {}
