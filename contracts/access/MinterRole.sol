237025ea0a1135cbc25c358cebe7a04d10b24b3b
Seth <hullogh@gmail.com>
2022-03-07 17:30:58 +0800

    refactor: clean up minter role

--- contracts/access/MinterRole.sol
+++ contracts/access/MinterRole.sol
@@ -1,17 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./IMinterRole.sol";
 
-interface IMinterRole {
-  function isMinter(address account) external view returns (bool);
-
-  function addMinter(address account) external;
-
-  function renounceMinter() external;
-}
-
-abstract contract MinterRole is IMinterRole, AccessControlEnumerable {
+abstract contract MinterRole is IMinterRole, AccessControl {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
   event MinterAdded(address indexed account);
@@ -22,8 +15,8 @@ abstract contract MinterRole is IMinterRole, AccessControlEnumerable {
     _setupRole(MINTER_ROLE, _msgSender());
   }
 
-  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable) returns (bool) {
-    return interfaceId == type(IMinterRole).interfaceId || AccessControlEnumerable.supportsInterface(interfaceId);
+  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl) returns (bool) {
+    return interfaceId == type(IMinterRole).interfaceId || AccessControl.supportsInterface(interfaceId);
   }
 
   modifier onlyMinter() {
