f6a87ba761854929d6a1617cd76abda37e030887
superical <superical@users.noreply.github.com>
2022-07-19 10:33:07 +0800

    test: chore work (#110)
    * feat: export assert from chai
    
    * feat: allow deploy-token fixture to use mock
    
    * fix: grammar
    
    * chore: update smock

--- test/fixtures/deploy-token.fixture.ts
+++ test/fixtures/deploy-token.fixture.ts
@@ -2,20 +2,23 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
 import { Contract, Signer } from "ethers";
 import { TitleEscrowFactory } from "@tradetrust/contracts";
+import { smock } from "@defi-wonderland/smock";
 
 export const deployTokenFixture =
-  <T extends Contract>({
+  <T extends Contract | unknown>({
     tokenContractName,
     tokenName,
     tokenInitials,
     deployer,
     escrowFactoryAddress = undefined,
+    useMock = false,
   }: {
     tokenContractName: string;
     tokenName: string;
     tokenInitials: string;
     deployer: SignerWithAddress | Signer;
     escrowFactoryAddress?: string;
+    useMock?: boolean;
   }) =>
   async () => {
     if (!escrowFactoryAddress) {
@@ -24,6 +27,13 @@ export const deployTokenFixture =
       // eslint-disable-next-line no-param-reassign
       escrowFactoryAddress = escrowFactoryContract.address;
     }
+
+    if (useMock) {
+      return (await (
+        await smock.mock(tokenContractName, deployer)
+      ).deploy(tokenName, tokenInitials, escrowFactoryAddress)) as unknown as T;
+    }
+
     const tradeTrustERC721Factory = await ethers.getContractFactory(tokenContractName);
     return (await tradeTrustERC721Factory
       .connect(deployer)
