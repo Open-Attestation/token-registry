0693d8ee9ef6219ead8f31f44c7ead43e90b4551
Seth <hullogh@gmail.com>
2022-11-16 11:16:28 +0800

    refactor: local ERC721Upgradeable.sol

--- contracts/interfaces/ITradeTrustERC721.sol
+++ contracts/interfaces/ITradeTrustERC721.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "../lib/token/IERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
 import "./ITitleEscrowFactory.sol";
 
