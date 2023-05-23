1ee72227851b5f9bbb5bd2674bbeceb49e1c12f7
AnilChinchawale <anil24593@gmail.com>
2023-05-23 16:18:04 +0530

    update contract address.

--- src/constants/contract-address.ts
+++ src/constants/contract-address.ts
@@ -15,7 +15,7 @@ export const contractAddress = {
     [ChainId.Sepolia]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
     [ChainId.Polygon]: "0xA38CC56c9291B9C1f52F862dd92326d352e710b8",
     [ChainId.PolygonMumbai]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
-    [ChainId.XDC]: "0xce28778bE6cF32ef3Ccbc09910258DF592F3b6F1",
+    [ChainId.XDC]: "0x50BfCc1b699fD2308B978B7a6A26e3C3Bbad16DC",
     [ChainId.XDCApothem]: "0xce28778bE6cF32ef3Ccbc09910258DF592F3b6F1",
   },
   Deployer: {
@@ -33,7 +33,7 @@ export const contractAddress = {
     [ChainId.Sepolia]: "0xC78BA1a49663Ef8b920F36B036E91Ab40D8F26D6",
     [ChainId.Polygon]: "0xd3F09dD800525Ecf7e452C3c167C7c716632d016",
     [ChainId.PolygonMumbai]: "0x83A533397eFE1d90baA26dEc7743626d7598656F",
-    [ChainId.XDC]: "0x5228b10FF132C1BFEa8bB08C10E8766f7938f289",
+    [ChainId.XDC]: "0x56E9b6B1C249de045Bd15016047E90c8641b6046",
     [ChainId.XDCApothem]: "0x5228b10FF132C1BFEa8bB08C10E8766f7938f289",
   },
 };
