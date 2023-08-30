f9495dbb716e4a0a818bc08974c2d1efe84b0735
superical <superical@users.noreply.github.com>
2023-08-30 12:25:06 +0800

    feat: update title escrow factory address on polygon (#173)

--- src/constants/contract-address.ts
+++ src/constants/contract-address.ts
@@ -13,7 +13,7 @@ export const contractAddress = {
     [ChainId.Ethereum]: "0xA38CC56c9291B9C1f52F862dd92326d352e710b8",
     [ChainId.Goerli]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
     [ChainId.Sepolia]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
-    [ChainId.Polygon]: "0xA38CC56c9291B9C1f52F862dd92326d352e710b8",
+    [ChainId.Polygon]: "0x5B5F8d94782be18E22420f3276D5ef5a1bc65C53",
     [ChainId.PolygonMumbai]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
     [ChainId.XDC]: "0x50BfCc1b699fD2308B978B7a6A26e3C3Bbad16DC",
     [ChainId.XDCApothem]: "0xce28778bE6cF32ef3Ccbc09910258DF592F3b6F1",
