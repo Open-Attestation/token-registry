44f6a8ae90615d135443781cd1775845b259c605
Seth <hullogh@gmail.com>
2023-10-13 18:58:58 +0800

    feat: remove goerli network

--- src/constants/contract-address.ts
+++ src/constants/contract-address.ts
@@ -1,6 +1,5 @@
 const ChainId = {
   Ethereum: 1,
-  Goerli: 5,
   Sepolia: 11155111,
   Polygon: 137,
   PolygonMumbai: 80001,
@@ -11,7 +10,6 @@ const ChainId = {
 export const contractAddress = {
   TitleEscrowFactory: {
     [ChainId.Ethereum]: "0xA38CC56c9291B9C1f52F862dd92326d352e710b8",
-    [ChainId.Goerli]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
     [ChainId.Sepolia]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
     [ChainId.Polygon]: "0x5B5F8d94782be18E22420f3276D5ef5a1bc65C53",
     [ChainId.PolygonMumbai]: "0x5aA71Cc9559bC5e54E9504a81496d9F8454721F5",
@@ -20,14 +18,12 @@ export const contractAddress = {
   },
   Deployer: {
     [ChainId.Ethereum]: "0x92470d0Fc33Cbf2f04B39696733806a15eD7eef3",
-    [ChainId.Goerli]: "0x9eBC30E7506E6Ce36eAc5507FCF0121BaF7AeA57",
     [ChainId.Sepolia]: "0x9eBC30E7506E6Ce36eAc5507FCF0121BaF7AeA57",
     [ChainId.Polygon]: "0x92470d0Fc33Cbf2f04B39696733806a15eD7eef3",
     [ChainId.PolygonMumbai]: "0x9eBC30E7506E6Ce36eAc5507FCF0121BaF7AeA57",
   },
   TokenImplementation: {
     [ChainId.Ethereum]: "0xd3F09dD800525Ecf7e452C3c167C7c716632d016",
-    [ChainId.Goerli]: "0xC78BA1a49663Ef8b920F36B036E91Ab40D8F26D6",
     [ChainId.Sepolia]: "0xC78BA1a49663Ef8b920F36B036E91Ab40D8F26D6",
     [ChainId.Polygon]: "0xd3F09dD800525Ecf7e452C3c167C7c716632d016",
     [ChainId.PolygonMumbai]: "0x83A533397eFE1d90baA26dEc7743626d7598656F",
